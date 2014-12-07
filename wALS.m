053a59f59545b620070b9018ebf61bc2e002f7b4
jian--huang <hj6569178@gmail.com>
2014-12-07 17:28:50 -0500

    Update wALS.m

--- wALS.m
+++ wALS.m
@@ -4,7 +4,7 @@ function [ X ] = wALS( R , d)
 %   d: rank
 %output
 %   X: low rank approximation
-
+tic
 [m,n] = size(R);
 W = R;
 lambda = 0.1;
@@ -13,7 +13,7 @@ delta = alpha*sum(sum(W))/sum(sum(W==0));
 W(W==0) = delta;
 U = zeros(m,d);
 V = normrnd(0,0.01,n,d);
-J_new = sum(sum(W.*((R-U*V').^2+lambda*(repmat(sum(U.*U,2),1,n)+repmat(sum(V'.*V'),n,1)))));
+J_new = sum(sum(W.*((R-U*V').^2+lambda*(repmat(sum(U.*U,2),1,n)+repmat(sum(V'.*V',1),m,1)))));
 while(1)
     J_old = J_new;
     for i = 1:m
@@ -22,9 +22,10 @@ while(1)
     for j = 1:n
         V(j,:) = R(:,j)'*diag(W(:,j))*U*(U'*diag(W(:,j))*U+lambda*sum(W(:,j))*eye(d))^-1;
     end
-    J_new = sum(sum(W.*((R-U*V').^2+lambda*(repmat(sum(U.*U,2),1,n)+repmat(sum(V'.*V'),n,1)))));
+    J_new = sum(sum(W.*((R-U*V').^2+lambda*(repmat(sum(U.*U,2),1,n)+repmat(sum(V'.*V',1),m,1)))));
     if abs(J_new-J_old) < J_old*10^-6
         break
     end
 end
 X = U*V';
+toc
